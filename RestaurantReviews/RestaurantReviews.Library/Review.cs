d3f6e66736d204e31849c88902e9cb92eaeba616
pjrivera08 <pj.rivera08@gmail.com>
2018-04-22 14:08:35 -0400

    Added Review class to Library Project

---
+++ RestaurantReviews/RestaurantReviews.Library/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Library
+{
+    class Review
+    {
+        Review()
+        {
+            reviewText = "";
+            reviewScore = 0;
+        }
+        private string reviewText;
+        private float reviewScore;
+    }
+}
