eac3721256885885fc36e751b920de3d0476604c
pjrivera08 <pj.rivera08@gmail.com>
2018-04-22 14:04:00 -0400

    Addded Restaurant Class to Library Project

---
+++ RestaurantReviews/RestaurantReviews.Library/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Library
+{
+    class Restaurant
+    {
+        public Restaurant(string _name, string _address)
+        {
+            restaurantName = _name;
+            restaurantAddress = _address;
+        }
+        private string restaurantName;
+        private string restaurantAddress;
+        private float customerRating;
+
+        private void setCustomerRating()
+        {
+            customerRating = 1; //PLACEHOLDER VALUE
+            return;
+        }
+    }
+}
