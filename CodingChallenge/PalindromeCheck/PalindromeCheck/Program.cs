7859b2eb276c5bf2c37775a54d61380d1ff37a64
pjrivera08 <pj.rivera08@gmail.com>
2018-04-23 11:26:57 -0400

    Added comments to code to explain logic

--- CodingChallenge/PalindromeCheck/PalindromeCheck/Program.cs
+++ CodingChallenge/PalindromeCheck/PalindromeCheck/Program.cs
@@ -16,39 +16,46 @@ namespace PalindromeCheck
             {
                 bool aIsLetterOrDigit = false;
                 bool bIsLetterOrDigit = false;
-                if(min > max)
+                
+                if (min > max)
                 {
-                    return true;
+                    return true; //If max gets lower than min then the string must be a palindrome
                 }
-                char a = 'A';
+                char a = 'A'; //Compiler didn't like me leaving these values blank
                 char b = 'B';
+
+                //Program will not assign the char from the string until it is confirmed that 
+                //the char is a Letter or a digit (not a punctuation mark)
                 while (!aIsLetterOrDigit)
                 {
                     if (Char.IsLetterOrDigit(str[min]))
                     {
                         a = str[min];
-                        aIsLetterOrDigit = true;
+                        aIsLetterOrDigit = true; //Allows the isLetterOrDigit loop to be exited
                     }
                     else
                     {
-                        min++;
+                        min++; //Skip the punctuation mark
                     }
                 }
+                //Program will not assign the char from the string until it is confirmed that 
+                //the char is a Letter or a digit (not a punctuation mark)
                 while (!bIsLetterOrDigit)
                 {
+                    
                     if (Char.IsLetterOrDigit(str[max]))
                     {
                         b = str[max];
-                        bIsLetterOrDigit = true; ;
+                        bIsLetterOrDigit = true; //Allows the isLetterOrDigit loop to be exited
                     }
                     else
                     {
-                        max--;
+                        max--; //Skip the punctuation mark
                     }
                 }
                 if (char.ToLower(a) != char.ToLower(b))
                 {
-                    return false;
+                    return false; //If the characters do not match then the string MUST NOT be a palindrome
                 }
                 min++;
                 max--;
@@ -56,9 +63,7 @@ namespace PalindromeCheck
         }
         static void Main(string[] args)
         {
-            
-
-            Console.ReadKey();
+          Console.ReadKey();
         }
     }
     
