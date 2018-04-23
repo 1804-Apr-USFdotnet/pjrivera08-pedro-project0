4d61beb4b56b620542640e060f131e453fcbdd66
pjrivera08 <pj.rivera08@gmail.com>
2018-04-23 11:21:29 -0400

    Added all test cases written on board

--- CodingChallenge/PalindromeCheck/PalindromeCheckTest/UnitTest1.cs
+++ CodingChallenge/PalindromeCheck/PalindromeCheckTest/UnitTest1.cs
@@ -9,16 +9,69 @@ namespace PalindromeCheckTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestPalindromeCheck()
+        public void TestPalindromeCheck0()
         {
-            string racecar = "racecar";
+            string test = "racecar";
             bool expected = true;
 
-            bool actual = Program.PalindromeCheckMethod(racecar);
+            bool actual = Program.PalindromeCheckMethod(test);
 
             Assert.AreEqual(expected, actual);
-            
+        }
 
+        [TestMethod]
+        public void TestPalindromeCheck1()
+        {
+            string test = "Racecar";
+            bool expected = true;
+
+            bool actual = Program.PalindromeCheckMethod(test);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPalindromeCheck2()
+        {
+            string test = "1221";
+            bool expected = true;
+
+            bool actual = Program.PalindromeCheckMethod(test);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPalindromeCheck3()
+        {
+            string test = "never Odd, or Even.";
+            bool expected = true;
+
+            bool actual = Program.PalindromeCheckMethod(test);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPalindromeCheck4()
+        {
+            string test = "1231";
+            bool expected = false;
+
+            bool actual = Program.PalindromeCheckMethod(test);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPalindromeCheck5()
+        {
+            string test = "aBc";
+            bool expected = false;
+
+            bool actual = Program.PalindromeCheckMethod(test);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
